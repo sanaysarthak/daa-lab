fa4fe1aa6fccb0d14b866bf5a21c58476b148a12
Sarthak Sanay <66907138+sanaysarthak@users.noreply.github.com>
2025-02-05 10:07:53 +0000

    add binary-search.c

---
+++ Binary Search/binary-search.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int binarySearch(int array[], int startIndex, int endIndex, int x)
+{
+  if (endIndex >= startIndex)  // used for checking if the position is at the extreme ends of the array or not
+  {
+    int mid = startIndex + (endIndex - startIndex) / 2;
+
+    // If found at mid, then return it
+    if(array[mid] == x)
+      return mid;
+
+    // Search the left half
+    else if(array[mid] > x) 
+      return binarySearch(array,  startIndex, mid - 1,x);
+
+    // Search the right half
+    else
+        return binarySearch(array, mid + 1, endIndex, x);
+  }
+  return -1;
+}
+
+int main() 
+{
+    int size, ele;
+    printf("Enter the size of an array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    printf("\nEnter elements in the array :-\n");
+    for(int i=0; i<size; i++)
+    {
+        printf("Enter element %d : ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nEnter a number to search it in the array: ");
+    scanf("%d", &ele);
+    printf("Performing Binary Search algorithm to check whether the number is in the array or not.\n");
+    
+    int res = binarySearch(arr, 0, size-1, ele);
+    if (res == -1)
+        printf("\nThe number %d is not in the Array.", ele);
+    else 
+        printf("\nThe number %d is present at index %d.\n", ele, res);
+    
+    return 0;
+}
